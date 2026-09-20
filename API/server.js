31e3d4e117afc97984975ae9c7032a4581493a0c
KForeman-Engineer <Kaydenforeman07@gmail.com>
2026-09-20 15:08:32 -0400

    Inital Commit

---
+++ API/server.js
@@ -0,0 +1,5 @@
+/**
+    Created: 2026-09-20
+    Purpose: Expose the ONIX API service to a local network to handle requests (main API point)
+    Author: Kayden Foreman
+ */
---
+++ main.py
@@ -0,0 +1,5 @@
+"""
+Created: 2026-09-20
+Purpose: Entry point for the ONIX API system. Starts the API Service to handle requests from API/server.js and respond with JSON data. takes terminal arguments to start the API Service
+Author: Kayden Foreman
+"""
